7ea45c711d778593f98abe81f591bcd11820c61a
zack6yuan <zack6yuan@gmail.com>
2024-11-01 16:37:11 -0500

    update number template and html.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" Module that starts a Flask web application """
-from flask import Flask
+""" Method: Start a Flask web application """
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -11,3 +13,26 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display(text):
+    return ("C {}".format(text))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
