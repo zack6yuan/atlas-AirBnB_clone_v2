1518b842fcab3e23ed72abbd79a3336dd6e9bf59
zack6yuan <zack6yuan@gmail.com>
2024-11-04 16:51:27 -0600

    update 6.

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Method: Start a Flask web application """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display(text):
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool")
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def template(n):
+    return render_template('6-number_odd_or_even.html', n=n)
