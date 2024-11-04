64933d7747e75902a24163ae57f88699621a417b
zack6yuan <zack6yuan@gmail.com>
2024-11-03 18:22:55 -0600

    update task 5.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,3 +35,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
         return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
