c110bc7c9fe92d96f08da2f753588d044c1b9eec
zack6yuan <zack6yuan@gmail.com>
2024-11-05 09:41:37 -0600

    update task 7.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Method: Start a Flask web application """
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -14,5 +16,9 @@ def teardown():
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    
-    return render_template('7-states_list.html', n=n)
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
