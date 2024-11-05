0c217fe55fb9d7c4ebba35dd1c6d132ac62b8913
zack6yuan <zack6yuan@gmail.com>
2024-11-05 14:49:49 -0600

    update task 10 py and html.

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Method: Start a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def display():
+    state = storage.all(State).values()
+    amenity = storage.all(State).values()
+    return render_template("10-hbnb_filters.html", state=state, amenity=amenity)
+
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
