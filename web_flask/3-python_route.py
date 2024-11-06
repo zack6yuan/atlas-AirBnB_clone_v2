d0d601a70b7a546df8abf81116b0caa38135fefd
zack6yuan <zack6yuan@gmail.com>
2024-11-06 16:51:54 -0600

    update doc.

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,22 +5,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+""" Root route """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return ("Hello HBNB!")
 
-
+""" HBNB route """
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return ("HBNB")
 
-
+""" Dynamic route - captures value from URL """
 @app.route('/c/<text>', strict_slashes=False)
 def display(text):
     return ("C {}".format(text.replace("_", " ")))
 
-
+""" Dynamic route - captures value from URL """
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
