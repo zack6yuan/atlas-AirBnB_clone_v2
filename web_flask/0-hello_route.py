d6a1a3488f7160991b9ba712176e295ae416e63e
zack6yuan <zack6yuan@gmail.com>
2024-11-06 16:37:04 -0600

    task 0 doc.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+""" Method: Root route """
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Method: Display "Hello HBNB!" to the user """
     return ("Hello HBNB!")
 
-
+""" Method: Start Flask web server"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
