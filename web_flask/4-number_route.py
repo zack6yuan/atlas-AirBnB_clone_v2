826b5ca6621a9d82c4977d666b0cda87f1b7677e
zack6yuan <zack6yuan@gmail.com>
2024-11-06 16:52:24 -0600

    update doc.

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,28 +5,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+""" Root route """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return ("Hello HBNB!")
 
-
+""" HBNB route """
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return ("HBNB")
 
-
+""" Dynamic route - captures value from URL """
 @app.route('/c/<text>', strict_slashes=False)
 def display(text):
     return ("C {}".format(text.replace("_", " ")))
 
-
+""" Dynamic route - captures value from URL """
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return ("Python {}".format(text.replace("_", " ")))
 
-
+""" Dynamic route - captures value from URL """
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     if not isinstance(n, int):
