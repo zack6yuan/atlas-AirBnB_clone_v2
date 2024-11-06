df7485e3c6612116df9c3512a8906ce1359fae61
zack6yuan <zack6yuan@gmail.com>
2024-11-06 16:38:43 -0600

    update doc.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 """ Method: Root route """
 @app.route('/', strict_slashes=False)
 def hello():
     """ Method: Display "Hello HBNB!" to the user """
     return ("Hello HBNB!")
+
 
 """ Method: Start Flask web server"""
 if __name__ == "__main__":
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,16 +5,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+""" Method: Root route """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Method: Display "Hello HBNB!" to the user """
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """ Method: Display "HBNB" to the user """
     return ("HBNB")
 
 
+""" Method: Start Flask web server"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
