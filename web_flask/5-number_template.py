ed11feb05ce2716d9f90495818a071749ee7fef8
zack6yuan <zack6yuan@gmail.com>
2024-11-06 17:36:54 -0600

    update doc.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,22 +8,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Method: Display "Hello HBNB!" to the user """
     return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Method: Display "HBNB!" to the user """
     return ("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display(text):
+    """ Method: Display "C" followed by text value """
     return ("C {}".format(text.replace("_", " ")))
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
+    """ Method: Display "Python" followed by text value """
     return ("Python {}".format(text.replace("_", " ")))
 
 
@@ -40,5 +44,6 @@ def template(n):
     return render_template('5-number.html', number=n)
 
 
+""" Method: Start Flask web server"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
