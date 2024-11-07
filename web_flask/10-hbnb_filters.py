a084b4bdda399e3c02ec42f809c4f9302947fd28
zack6yuan <zack6yuan@gmail.com>
2024-11-06 21:59:40 -0600

    final push.

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -10,6 +10,7 @@ from models.amenity import Amenity
 app = Flask(__name__)
 
 
+""" HBNB filters route """
 @app.route('/hbnb_filters', strict_slashes=False)
 def display():
     state = storage.all(State).values()
@@ -17,11 +18,13 @@ def display():
     return render_template("10-hbnb_filters.html", state=state, amenity=amenity)
 
 
+""" TearDown """
 @app.teardown_appcontext
 def teardown():
     """ Method: Remove the current SQLAlchemy session """
     storage.close()
 
 
+""" Method: Start Flask web server """
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
